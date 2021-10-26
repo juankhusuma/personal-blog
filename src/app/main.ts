ae05a7be6b02c4ee374a287db39386ec55daa9c5
Juan D.K <66712975+Juan-d-khusuma@users.noreply.github.com>
2021-10-26 20:59:00 +0700

    After a long winded debate with myself, I finally rested my fate on pug

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,1 +1,17 @@
-console.log('HEllo world');
+import express from 'express';
+import morgan from 'morgan';
+
+const port = process.env.PORT || 5500;
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.static('views'));
+
+app.set('view engine', 'pug');
+
+app.get('/', (_, res) => {
+	res.render('index', { name: 'John' });
+});
+
+
+app.listen(port, () => console.log(`Server started at port ${port} 🤘`));
