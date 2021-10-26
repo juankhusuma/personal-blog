5979396073ba79c9b7c01acd689ac724755c1c67
Juan D.K <66712975+Juan-d-khusuma@users.noreply.github.com>
2021-10-26 22:21:27 +0700

    sprinkled in Prisma ORM to the project, also created a CRUD functionality for the user

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,11 +1,17 @@
 import express from 'express';
 import morgan from 'morgan';
+import UserRouter from '../routes/user';
+import bp from 'body-parser';
 
-const port = process.env.PORT || 5500;
+const port = process.env.PORT || 6500;
 const app = express();
 
 app.use(morgan('dev'));
 app.use(express.static('views'));
+app.use(bp.json());
+
+/** Router Registrations */
+app.use('/', UserRouter);
 
 app.set('view engine', 'pug');
 
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,5 +1,61 @@
 import express from 'express';
+import { Prisma, PrismaClient } from '@prisma/client';
 
-const router = express.Router({
+const prisma = new PrismaClient();
+
+const UserRouter = express.Router({
 	caseSensitive: true,
 });
+
+/** Get all users */
+UserRouter.get('/user', async (_, res, next) => {
+	try {
+		const users = await prisma.user.findMany(); 
+		return res.json(users);
+	} catch (error) {
+		return next(error);
+	}
+});
+/** Get user by id */
+UserRouter.get('/user/:id', async (req, res, next) => {
+	try {
+		const id = parseInt(req.params.id);
+		const user = await prisma.user.findUnique({ where: { id } });
+		return res.json(user);
+	} catch (error) {
+		return next(error);
+	}
+});
+/** Creates new user */
+UserRouter.post('/user', async (req, res, next) => {
+	try {
+		const userInput = req.body as Prisma.UserCreateInput;
+		const user = await prisma.user.create({ data: {...userInput} });
+		return res.json(user);
+	} catch (error) {
+		return next(error);
+	}
+});
+/** Update an user */
+UserRouter.put('/user/:id', async (req, res, next) => {
+	try {
+		const id = parseInt(req.params.id);
+		const userUpdateInput = req.body as Prisma.UserUpdateInput; 
+		const user = await prisma.user.update({ where: { id }, data: {...userUpdateInput} });
+		return res.json(user);
+	} catch (error) {
+		return next(error);
+	}
+});
+/** Delete an user */
+UserRouter.delete('/user/:id', async (req, res, next) => {
+	try {
+		const id = parseInt(req.params.id);
+		const user = await prisma.user.delete({ where: { id } });
+		return res.json(user);
+	} catch (error) {
+		return next(error);
+	}
+});
+
+export default UserRouter;
