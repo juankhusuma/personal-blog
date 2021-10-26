3a217c32ebc96e2806fb12f92b107f4b7d859826
Juan D.K <66712975+Juan-d-khusuma@users.noreply.github.com>
2021-10-26 22:57:04 +0700

    Integrated tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	// mode: 'jit',
+	// purge: {
+	// 	enabled: true,
+	// 	content: ['views/**/*.pug']
+	// },
+	darkMode: 'class', // or 'media' or 'class'
+	theme: {
+		extend: {},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
