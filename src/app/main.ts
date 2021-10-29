2a1d2a0f6bb3260de1c444731794dde7debd3f50
Juan D.K <66712975+Juan-d-khusuma@users.noreply.github.com>
2021-10-29 13:43:31 +0700

    The MVC model started to come together now, hopefully

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,23 +1,45 @@
 import express from 'express';
 import morgan from 'morgan';
-import UserRouter from '../routes/user';
 import bp from 'body-parser';
+import { PrismaClient } from '@prisma/client';
 
 const port = process.env.PORT || 6500;
 const app = express();
+const prisma = new PrismaClient();
 
 app.use(morgan('dev'));
 app.use(express.static('views'));
-
 app.use(bp.json());
-
-/** Router Registrations */
-app.use('/', UserRouter);
 
 app.set('view engine', 'pug');
 
-app.get('/', (_, res) => {
-	res.render('index');
+app.get('/', async (_, res) => {
+	const posts = await prisma.post.findMany({
+		include: {
+			user: {
+				select: {
+					username: true
+				}
+			}
+		}
+	});
+	res.status(200).render('index', { posts });
+});
+
+app.get('/post/:id', async (req, res) => {
+	const post = await prisma.post.findUnique({
+		where: {
+			id: parseInt(req.params.id) 
+		},
+		include: {
+			user: {
+				select: {
+					username: true 
+				} 
+			} 
+		} 
+	});
+	res.status(200).render('post', { post });
 });
 
 
